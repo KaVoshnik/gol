061ae8d8d71e3feb06071a30efa431953e1cae5f
kavoshnik <hikkan008@gmail.com>
2025-03-15 11:59:10 +0700

    start work

---
+++ game_of_life.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("goida");
+
+  return 0;
+}
+
+void init_world() {}
+
+void print_world() {}
+
+int get_live_count() {}
+
+void read_point_neighbors() {}
+
+int count_live_neighbors() {}
+
+void next_gen() {}
+
+void copy_world() {}
+
+int cmp_world() {}
